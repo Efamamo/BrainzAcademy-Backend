928f1372e593c16867542bfeee76a871d9411852
ephrem <ephremmamo555@gmail.com>
2024-09-04 10:19:58 +0300

    added cors

--- server.mjs
+++ server.mjs
@@ -2,9 +2,9 @@ import dotenv from "dotenv"
 import mongoose from "mongoose";
 import express from "express"
 import { authRouter } from "./apis/routes/auth.mjs";
+import cors from 'cors'
 
 dotenv.config()
-
 
 mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
@@ -14,6 +14,7 @@ db.once("open",()=>{console.log("connected")})
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use("/auth", authRouter)
 app.listen(3000)
